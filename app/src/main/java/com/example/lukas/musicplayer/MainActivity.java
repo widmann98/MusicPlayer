d0688f0801a2ee62b8e9ae6f1822c7de0a0073e9
widmann98 <lukiwidi@gmail.com>
2018-02-28 10:31:45 +0100

    .

---
+++ app/src/main/java/com/example/lukas/musicplayer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lukas.musicplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button songs = (Button) findViewById(R.id.songs);
+        songs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent sett = new Intent(MainActivity.this, SongActivity.class);
+                MainActivity.this.startActivity(sett);
+            }
+        });
+        Button playlist = (Button) findViewById(R.id.playlist);
+        playlist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent sett = new Intent(MainActivity.this, PlaylistActivity.class);
+                MainActivity.this.startActivity(sett);
+            }
+        });
+    }
+}
