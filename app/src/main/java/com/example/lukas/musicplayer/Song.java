4dbf7cff6df8a16bd0085f08dc931043e0e8ca6a
widmann98 <lukiwidi@gmail.com>
2018-02-28 14:43:19 +0100

    lol

--- app/src/main/java/com/example/lukas/musicplayer/Song.java
+++ app/src/main/java/com/example/lukas/musicplayer/Song.java
@@ -1,11 +1,25 @@
 package com.example.lukas.musicplayer;
 
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Lukas on 28.02.18.
  */
 
 public class Song {
 
-    private String title;
-    private String interpreter;
+    private String TVtitle;
+    private String TVinterpreter;
+
+    public String getTitle() {
+        return TVtitle;
+    }
+
+    public String getInterpreter() {
+        return TVinterpreter;
+    }
+
+
 }
+
