548957e1922bc266a769a71506fb0f46444c8085
widmann98 <lukiwidi@gmail.com>
2018-03-07 12:40:12 +0100

    lol

--- app/src/main/java/com/example/lukas/musicplayer/SongAdapter.java
+++ app/src/main/java/com/example/lukas/musicplayer/SongAdapter.java
@@ -1,12 +1,16 @@
 package com.example.lukas.musicplayer;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
 /**
  * Created by Lukas on 28.02.18.
  */
@@ -14,8 +18,8 @@ import java.util.ArrayList;
 public class SongAdapter extends ArrayAdapter<Song>{
 
     private static class ViewHolder{
-        public TextView title;
-        public TextView interpreter;
+        public TextView tvTitle;
+        public TextView tvIinterpreter;
     }
 
 
