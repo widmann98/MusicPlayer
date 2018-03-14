2d1864f55de526bec9a09bc53800050edcb07d44
MaXX888 <36234094+MaXX888@users.noreply.github.com>
2018-03-14 14:20:45 +0100

    lol

--- app/src/main/java/com/example/lukas/musicplayer/ItemPlaylistActivity.java
+++ app/src/main/java/com/example/lukas/musicplayer/ItemPlaylistActivity.java
@@ -2,12 +2,24 @@ package com.example.lukas.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ItemPlaylistActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private PlaylistAdapter playlistAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+
+        listView = (ListView)findViewById(R.id.playlist_list);
+        ArrayList<Playlist> playlists = new ArrayList<>();
+        playlistAdapter = new PlaylistAdapter(this, playlists);
+        listView.setAdapter(playlistAdapter);
+
     }
 }
